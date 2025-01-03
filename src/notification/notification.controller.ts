8f8b12a3b16b83a2720085dfe1d2779225c7ea5c
MD Ariful Islam Protik <mdarifulislam.protik@gmail.com>
2025-01-03 10:49:53 +0600

    add: notification controller

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Query } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import { NotificationService } from './notification.service';
 
 @Controller('notification')
-export class NotificationController {}
+export class NotificationController {
+  constructor(private notificationService: NotificationService) {}
+  @Get()
+  getNotifications(
+    @GetUser() user: User,
+    @Query('cursor') cursor?: string,
+    @Query('take') take?: number,
+  ) {
+    return this.notificationService.getNotifications(user.id, cursor, take);
+  }
+  @Post()
+  markAsRead(@GetUser() user: User) {
+    return this.notificationService.markAsRead(user.id);
+  }
+}
--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -30,4 +30,55 @@ export class NotificationService {
     });
     this.notificationGateway.sendNotification(n);
   }
+  // getNotifications with cursor-based pagination and return with count and next cursor also return unread notification count for the user
+  async getNotifications(userId: string, cursor?: string, take = 10) {
+    const notifications = await this.prismaService.notification.findMany({
+      where: {
+        receiverId: userId,
+      },
+      take: take + 1,
+      skip: cursor ? 1 : 0,
+      cursor: cursor ? { id: cursor } : undefined,
+      orderBy: {
+        createdAt: 'desc',
+      },
+      include: {
+        actor: {
+          select: {
+            id: true,
+            username: true,
+            name: true,
+          },
+        },
+      },
+    });
+    const unreadCount = await this.prismaService.notification.count({
+      where: {
+        receiverId: userId,
+        isRead: false,
+      },
+      take: take + 1,
+    });
+    const hasMore = notifications.length === take + 1;
+    return {
+      notifications: hasMore ? notifications.slice(0, -1) : notifications,
+      count: notifications.length,
+      nextCursor: hasMore ? notifications[take - 1].id : null,
+      unreadCount,
+    };
+  }
+  // mark notification as read batch operation like latest 50 notification
+  async markAsRead(userId: string) {
+    await this.prismaService.notification.updateMany({
+      where: {
+        receiverId: userId,
+        isRead: false,
+      },
+
+      data: {
+        isRead: true,
+      },
+    });
+    return { message: 'Marked as read' };
+  }
 }
